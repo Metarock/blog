3912b3f9c36d444ea369ede1d49bf2cac124ddbb
Metarock <esang037@gmail.com>
2023-04-22 18:00:05 +1200

    use next-theme

--- src/components/wrappers/themeProvider.tsx
+++ src/components/wrappers/themeProvider.tsx
@@ -1,11 +1,26 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { useEffect, useState } from "react";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export default function Providers({ children }: Props) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  const [mount, setMount] = useState(false);
+
+  useEffect(() => {
+    setMount(true);
+  }, []);
+
+  if (!mount) {
+    return <>{children}</>;
+  }
+
+  return (
+    <ThemeProvider enableSystem attribute="class">
+      {children}
+    </ThemeProvider>
+  );
 }
